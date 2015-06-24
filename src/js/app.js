b682c2d8f7e01c31ec0cd74cc78f21bf2c672603
Donielle Berg <d.berg@salesforce.com>
2015-06-24 12:18:03 -0700

    Add more data and set up react

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+let posts = [
+  { name: "Jay Z", avatar: "jayZ", timestamp: "1h", image: "warriors-parade", isFavorited: false },
+  { name: "Beyonce", avatar: "beyonce", timestamp: "2h", image: "beach", isFavorited: false },
+  { name: "Janelle", avatar: "janelle", timestamp: "3h", image: "water-color", isFavorited: false },
+];
+
+
 let App = React.createClass({
   displayName: "App",
   render() {
